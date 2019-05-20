5ef319b266428ab2525996ddd73eb601de750bc3
Gustaf Folkmar <gustaf@folkmar.se>
2019-05-20 19:27:19 -0400

    Created migration to add post reference to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_225423) do
+ActiveRecord::Schema.define(version: 2019_05_20_232308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_225423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "price"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_bookings_on_post_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_225423) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "posts"
   add_foreign_key "bookings", "users"
   add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
