992e3affdc7c2870167d8ca847d8428f5b1a9406
Jose Zea <josedavid.zea@gmail.com>
2019-05-20 17:07:54 -0400

    changed the tests for correct views

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -11,21 +11,6 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
-    get posts_create_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get posts_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get posts_destroy_url
-    assert_response :success
-  end
-
   test "should get show" do
     get posts_show_url
     assert_response :success
