1fa169e98ae3fc5e16de134efa2e7b9df860e9c2
Jose Zea <josedavid.zea@gmail.com>
2019-05-20 15:28:19 -0400

    added new column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_182758) do
+ActiveRecord::Schema.define(version: 2019_05_20_192645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_182758) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "price"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
