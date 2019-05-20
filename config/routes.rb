d31370a7c587ec0aece96453e4d4b5d6c576f095
Gustaf Folkmar <gustaf@folkmar.se>
2019-05-20 16:32:16 -0400

    removed booking destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  resources :bookings, except: [:new, :create]
+  resources :bookings, except: [:new, :create, :destroy]
 
   resources :posts do
     resources :photos, only: [:new, :create, :destroy]
