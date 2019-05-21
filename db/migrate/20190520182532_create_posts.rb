4cbfc1dfe025d6641539e559c62dde0f722e4870
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-21 10:28:15 -0400

    added category column in migration file

--- db/migrate/20190520182532_create_posts.rb
+++ db/migrate/20190520182532_create_posts.rb
@@ -5,6 +5,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :address
       t.string :price
       t.string :title
+      t.string :category
       t.references :user, foreign_key: true
 
       t.timestamps
