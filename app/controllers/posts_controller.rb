cad7c4cdcbd17c294044750b49d5da99ea5c4226
Jose Zea <josedavid.zea@gmail.com>
2019-05-21 11:55:25 -0400

    finished the new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,23 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.photos.build
   end
 
   def create
+    @post = Post.new(title: params['post'][:title],
+                     description: params['post'][:description],
+                     address: params['post'][:address],
+                     price: params['post'][:price],
+                     user: current_user)
+    if @post.save
+      @photo = Photo.new(source: params['post']['photos_attributes']['0']['source'],
+                         post: @post)
+      @photo.save
+      redirect_to post_path(@post.id)
+    else
+      redirect_to new_post_path
+    end
   end
 
   def update
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :photos
+
+  accepts_nested_attributes_for :photos
 end
