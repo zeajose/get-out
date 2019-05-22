db28c87548c452e9824594d4091f0f07ef2c7ca4
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-22 10:36:18 -0400

    basic JS for featured card redirect, may need some changes

---
+++ app/javascript/components/landingPage.js
@@ -0,0 +1,9 @@
+function redirectOnClick() {
+
+const selectedCard = document.getElementById("featured-card");
+
+selectedCard.addEventListener("click", (event) => {
+redirect_to "post_path"
+});
+
+}
