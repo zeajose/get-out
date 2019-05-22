2918be08e69dbf0254a868371158267ef3edb30c
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-22 10:55:56 -0400

    cards now linkable to their own show page

--- app/javascript/components/landingPage.js
+++
@@ -1,9 +0,0 @@
-function redirectOnClick() {
-
-const selectedCard = document.getElementById("featured-card");
-
-selectedCard.addEventListener("click", (event) => {
-redirect_to "post_path"
-});
-
-}
