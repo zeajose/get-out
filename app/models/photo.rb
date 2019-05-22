2165a3e86495d2b8e6200109b186dbe5eee563d8
Joe Di Lillo <jdilillo@gmail.com>
2019-05-22 13:38:15 -0400

    Fixing Maps

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,4 @@
 class Photo < ApplicationRecord
   belongs_to :post
   mount_uploader :source, PhotoUploader
-  validates :photo, presence: true
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  validates :photo, presence: true
 
   belongs_to :user
   has_many :bookings
