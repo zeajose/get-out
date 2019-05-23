6cc53917520ae8eddd49e2670217f1d59fafa16b
Joe Di Lillo <jdilillo@gmail.com>
2019-05-23 14:34:54 -0400

    Minor Changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/initAutocomplete';
 
 initMapbox();
+initAutocomplete();
