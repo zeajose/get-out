85b7d04314125efeac64f9c1ffd7c0f0f28fdf8f
Thomas Ferris <thomaskeithferris@gmail.com>
2019-05-23 11:35:00 -0400

    quick dirty fix w/ payment button display

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'search', to: 'posts#search', as: :search_posts
-
   get 'payment', to: 'bookings#payment', as: :payment
 
   resources :bookings, except: [:new, :create, :destroy]
