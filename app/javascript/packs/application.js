4e208b47b123fe3863f4a8abe0ee62db6c8915a6
Joe Di Lillo <jdilillo@gmail.com>
2019-05-23 14:49:59 -0400

    Adding autocomplete and back button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/initAutocomplete';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
 initAutocomplete();
